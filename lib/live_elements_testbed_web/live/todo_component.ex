60aa445b056dbe4695f699583c06214e98f57ba7
Chris Nelson <superchrisnelson@gmail.com>
2023-03-22 19:36:25 -0400

    works in components too

--- lib/live_elements_testbed_web/live/todo_component.ex
+++ lib/live_elements_testbed_web/live/todo_component.ex
@@ -1,15 +1,16 @@
-defmodule LiveElementsTestbedWeb.TodoLive do
-  use LiveElementsTestbedWeb, :live_view
+defmodule LiveElementsTestbedWeb.TodoComponent do
+  use LiveElementsTestbedWeb, :live_component
 
   use LiveElements.CustomElementsHelpers
 
   @impl true
-  def mount(_params, _session, socket) do
+  def update(assigns, socket) do
     {:ok, socket |> assign(todos: ["hey"])}
   end
 
   @impl true
   def handle_event("add_todo", %{"todo" => todo}, %{assigns: %{todos: todos}} = socket) do
+    IO.inspect(todo)
     {:noreply, socket |> assign(todos: todos ++ [todo])}
   end
 
--- test/live_elements_testbed_web/features/todo_list_test.exs
+++ test/live_elements_testbed_web/features/todo_list_test.exs
@@ -16,4 +16,16 @@ defmodule LiveElementsTestbedWeb.Features.TodoListTest do
     end)
   end
 
+  feature "in a component", %{session: session} do
+    session
+    |> visit("/with_component")
+    |> assert_text("todo list")
+    |> within_shadow_dom("todo-list", fn shadow_dom ->
+      shadow_dom
+      |> fill_in(css("input[name='todo']"), with: "Do a thing")
+      |> click(css("button"))
+      |> assert_has(css("li", text: "Do a thing"))
+    end)
+  end
+
 end
